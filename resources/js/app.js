0daa196378e643a9d9e962bd55d9d780f98938a8
supportteem <pkumarrana143@gmail.com>
2025-05-06 12:31:54 +0530

    user authentication added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,11 @@
 import './bootstrap';
+import '../css/app.css';
 import $ from 'jquery';
 import 'slick-carousel';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 window.$ = window.jQuery = $;  // Expose jQuery globally
+
 
 
 $(document).ready(function () {
